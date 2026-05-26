2f8fb98a3758be2ab0d7ca63f0c4ffc83cf662d1
ayoisrael@outlook.com <ayoisrael@outlook.com>
2026-05-26 15:10:29 +0100

    Update PlanetCard and PlanetGrid styles

--- src/components/PlanetGrid/PlanetCard.jsx
+++ src/components/PlanetGrid/PlanetCard.jsx
@@ -12,15 +12,15 @@ function PlanetCard({ planet }) {
       <figcaption className="planet-card__caption">
         <h3 className="planet-card__name">{planet.planet}</h3>
         <p className="planet-card__distance">
-          <span className="planet-card__distance-label">Distance from Sun</span>
+          {/* <span className="planet-card__distance-label">Distance from Sun</span> */}
           <span className="planet-card__distance-value">
-            {planet.distanceFromSun}{' '}
+            {planet.distanceFromSun}{" "}
             <abbr title="million kilometres">M km</abbr>
           </span>
         </p>
       </figcaption>
     </figure>
-  )
+  );
 }
 
-export default PlanetCard
+export default PlanetCard;
